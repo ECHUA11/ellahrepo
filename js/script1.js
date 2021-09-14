2f00c4e2c13e15b4582aeb56aff75d0613834420
ECHUA11 <90659632+ECHUA11@users.noreply.github.com>
2021-09-14 10:42:43 +0800

    answer to question1 &2

---
+++ js/script1.js
@@ -0,0 +1,9 @@
+var myself={firstame="Ellah Nicole",lastname="Chua",age="21",subject="ICS2609"}
+
+enrolled: function(){
+console.log(' Hello, I am '${myself.firstname})}
+introduce: function(){
+console.log(' Hello, I am enrolled in '${myself.subject}')}
+};
+console.log(myself.erolled());
+console.log(myself.introduce());
